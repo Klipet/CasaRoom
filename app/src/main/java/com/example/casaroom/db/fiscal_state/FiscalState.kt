ddd46b558593771d7a5e78985395db6e6da00f13
Iura ttttt <iurik1989@gmail.com>
2024-01-16 18:02:45 +0200

    wtf

--- app/src/main/java/com/example/casaroom/db/fiscal_state/FiscalState.kt
+++ app/src/main/java/com/example/casaroom/db/fiscal_state/FiscalState.kt
@@ -1,22 +1,8 @@
 package com.example.casaroom.db.fiscal_state
 
 data class FiscalState(
-    val CurrentReport24HoursExpired: Boolean,
-    val CurrentReportDate: String,
-    val CurrentReportNumber: Int,
-    val FirstNotSyncedTransactionDate: Any,
-    val FirstNotSyncedTransactionNumber: Any,
-    val FiscalMemorySerialNumber: String,
-    val LastTransactionDate: String,
-    val LastTransactionMEVID: String,
-    val LastTransactionNumber: Int,
-    val OfflineHoursLimit: Int,
-    val OfflineModeEnabled: Boolean,
-    val ServiceDatabaseCorrupted: Boolean,
-    val ServiceDateTime: String,
-    val ServiceDatetimeInvalid: Boolean,
-    val ServiceMEVSyncError: Boolean,
-    val ServiceRegistrationInvalid: Boolean,
-    val ServiceSerialNumber: String,
-    val ServiceSignatureInvalid: Boolean
+    val Status: String,
+    val deviceId: String,
+    val recipesWithoutMev: Int,
+    val tva: List<Tva>
 )
